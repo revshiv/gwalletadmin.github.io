c311d678973182fe7b8b7c1f57522be57c6fe58b
shiv <shiv@revinfotech.com>
2019-03-27 16:40:01 +0530

    gwallet admin

---
+++ precache-manifest.f9998ff99c2ccdc9faf9d7d425907fad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e48db17e4fad1120ae32",
+    "url": "/static/js/main.0690fa18.chunk.js"
+  },
+  {
+    "revision": "3054620fbd71c553db91",
+    "url": "/static/js/2.3bcdc0cc.chunk.js"
+  },
+  {
+    "revision": "e48db17e4fad1120ae32",
+    "url": "/static/css/main.12422099.chunk.css"
+  },
+  {
+    "revision": "3700b7324cd23fccef2cfcef45045ff0",
+    "url": "/index.html"
+  }
+];
